e6af9a72e192bf408d456628d42b8f42129ee163
repinnick <check.point9917@yandex.ru>
2022-09-15 17:05:36 +0300

    feat: basic project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<routerlink-outlet></routerlink-outlet>'
 })
 export class AppComponent {
-  title = 'data-list';
 }
